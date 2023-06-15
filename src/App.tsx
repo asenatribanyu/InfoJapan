e4fb4aadcba793fbdd234e3cac8d91cbb1f6863e
zaldyalfarez <zaldyalfarez@gmail.com>
2023-06-15 13:17:51 +0700

    Menu 3 Done

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {default as mapping} from '../mapping.json';
 import BahasaJepang from './pages/bahasaJepang';
 import BudayaJepang from './pages/budayaJepang';
+import KesempatanJepang from './pages/kesempatanJepang';
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light} customMapping={mapping}>
-      {/* <BahasaJepang /> */}
-      <BudayaJepang />
+      {/* <BahasaJepang />
+      <BudayaJepang /> */}
+      <KesempatanJepang />
     </ApplicationProvider>
   </>
 );
